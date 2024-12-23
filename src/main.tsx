8e9d9cdc310e702897b5f0ceb2b4025e2bff562a
Ere Männistö <connect@eremannisto.com>
2024-12-23 19:28:45 +0100

    Chore: Add main layout

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import '@styles/reset.css'
+import '@styles/typography.css'
+import '@styles/colors.css'
+import '@styles/global.css'
+
+import Navigation  from '@components/Navigation/Navigation.tsx'
+import TaskManager from '@components/TaskManager/TaskManager.tsx'
+import Background  from '@components/Background/Background.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Navigation />
+    <main>
+      <TaskManager />
+    </main>
+    <Background />
+  </StrictMode>,
+)

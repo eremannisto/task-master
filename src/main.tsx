aab2bd36154b85fcca8a180ba9002d43b49be301
Ere Männistö <connect@eremannisto.com>
2024-12-31 03:51:42 +0200

    Fix: Add correct import paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import '@styles/reset.css'
-import '@styles/typography.css'
-import '@styles/colors.css'
-import '@styles/global.css'
+import '@/styles/reset.css'
+import '@/styles/typography.css'
+import '@/styles/colors.css'
+import '@/styles/global.css'
 
 import Navigation  from '@/components/Navigation/Navigation.component'
-import TaskManager from '@components/TaskManager/TaskManager.component'
+import TaskManager from '@/components/TaskManager/TaskManager.component'
 import Background  from '@/components/Background/Background.component'
 
 function App() {
